f9e0964cc78784ff8ae183aedde6473a0b307637
Kp <kp_1@outlook.com>
2021-06-11 17:23:13 +0400

    Added folders in components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './components/About/About';
+// import Navbar from './components/Navbar';
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
+import Footer from './components/Footer';
+// import './style.css';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
