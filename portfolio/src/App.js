251bc5307ff907afb226c8b83f4d58e27bc806a0
Kp <kp_1@outlook.com>
2021-06-13 21:34:21 +0400

    Revamped to make it simpler

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,39 +1,30 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import About from './components/About/About';
+
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 import Navbar from './components/Navbar';
-import Home from './components/Home/Home';
-import Projects from './components/Projects/Projects';
-import Footer from './components/Footer';
-// import './style.css';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Resume from './components/Resume/Resume';
 
 function App() {
-  const [load, upadateLoad] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-  }, []);
-
   return (
-    <Router>
-      <div className='App' id={load ? 'no-scroll' : 'scroll'}>
-        <Navbar />
-
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/project' component={Projects} />
-          <Route path='/about' component={About} />
-          <Route path='/resume' component={Resume} />
-        </Switch>
-        <Footer />
+    <div className='App'>
+      <Navbar></Navbar>
+      <div className='App-header'>
+        <Header></Header>
       </div>
-    </Router>
+
+      <div className='About'>
+        <About></About>
+      </div>
+      <div className='portfolio'>
+        <Portfolio></Portfolio>
+      </div>
+      <div className='contact'>
+        <Contact></Contact>
+      </div>
+    </div>
   );
 }
 
